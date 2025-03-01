e6fb361e57224d758cfbfb3dd5e331b2598269b3
bornohi2019n <bornohin2019@gmail.com>
2025-03-01 17:04:50 +0600

    fix all eroor

--- resources/views/dashboard/index.blade.php
+++ resources/views/dashboard/index.blade.php
@@ -16,8 +16,8 @@
         <div class="col-lg-4">
             <div class="card card-transparent card-block card-stretch card-height border-none">
                 <div class="card-body p-0 mt-lg-2 mt-0">
-                    <h3 class="mb-3">Hi {{ auth()->user()->name }}, Good Morning</h3>
-                    <p class="mb-0 mr-4">Your dashboard gives you views of key performance or business process.</p>
+                    <h3 class="mb-3">Meghdoot IT</h3>
+                    <p class="mb-0 mr-4">Meghdoot IT's dashboard offers a seamless and intuitive view of key performance metrics and business processes, empowering smarter decisions with real-time insights.</p>
                 </div>
             </div>
         </div>
